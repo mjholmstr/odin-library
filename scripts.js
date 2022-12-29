d0ed80e93f3e1aba1b2f8b034c05c6bcfefb0a29
Matt Holmstrom <mjholmstr@gmail.com>
2022-12-29 16:10:11 -0600

    Add submit/reset buttons on form

--- scripts.js
+++ scripts.js
@@ -22,6 +22,11 @@ function addBookToLibrary(title, author, pages, read) {
 function displayBooksOnPage() {
     const books = document.querySelector(".books");
 
+    const removeDivs = document.querySelectorAll(".card");
+    for (let i = 0; i < removeDivs.length; i++) {
+        removeDivs[i].remove();
+    }
+
     myLibrary.forEach(myLibrary => {
         const card = document.createElement("div");
         card.classList.add("card");
@@ -43,12 +48,42 @@ function displayForm() {
     document.getElementById("add-book-form").style.display = "";
 }
 
-addBookToLibrary("The Hobbit", "Tolkien", "295", "Not");
-addBookToLibrary("Liberation Day", "GeorgeSaunders", "242", "Read");
-addBookToLibrary("The Hobbit", "Tolkien", "295", "Not");
-addBookToLibrary("Liberation Day", "GeorgeSaunders", "242", "Read");
-addBookToLibrary("The Hobbit", "Tolkien", "295", "Not");
-addBookToLibrary("Liberation Day", "GeorgeSaunders", "242", "Read");
+// Add input to array for new form entry
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", intakeFormData);
+
+function intakeFormData() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").value;
+
+    // Break out if form is not complete
+    if ((title == "") || (author == "") || (pages == "") || (read == "")) {
+        return;
+    }
+
+    addBookToLibrary(title, author, pages, read);
+
+    document.getElementById("add-book").reset();
+}
+
+// Clear/reset form
+const clearButton = document.querySelector(".reset-button");
+clearButton.addEventListener("click", clearForm);
+
+function clearForm() {
+    document.getElementById("add-book").reset();
+}
+
+
+
+// addBookToLibrary("The Hobbit", "Tolkien", "295", "Not");
+// addBookToLibrary("Liberation Day", "GeorgeSaunders", "242", "Read");
+// addBookToLibrary("The Hobbit", "Tolkien", "295", "Not");
+// addBookToLibrary("Liberation Day", "GeorgeSaunders", "242", "Read");
+// addBookToLibrary("The Hobbit", "Tolkien", "295", "Not");
+// addBookToLibrary("Liberation Day", "GeorgeSaunders", "242", "Read");
 
 console.log("End of code array contents", myLibrary);
 
